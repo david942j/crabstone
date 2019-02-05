8e836c957f29dd6040b6bf61eb530404da7d0260
david942j <david942j@gmail.com>
2019-02-05 04:24:14 -0800

    Fully test m68k

--- spec/arch/m68k_spec.rb
+++ spec/arch/m68k_spec.rb
@@ -17,6 +17,7 @@ describe 'Crabstone::M68K' do
     # fadd.s  #3.141500, fp0
     op = op_of("\xf2\x3c\x44\x22\x40\x49\x0e\x56", 1)
     expect(op.valid?).to be true
+    expect(op.reg?).to be true
     expect(@cs.reg_name(op.value)).to eq 'fp0'
   end
 
@@ -45,15 +46,35 @@ describe 'Crabstone::M68K' do
   end
 
   it 'fp_double' do
+    # fadd.d #2.718280, fp0
+    op = op_of("\xf2\x3c\x54\x22\x40\x05\xbf\x09\x95\xaa\xf7\x90", 0)
+    expect(op.fp_double?).to be true
+    # There's a bug of Casptone 4, the fixes PR is here: aquynh/capstone#1369,
+    # so we simply check the type but not the value.
+    expect(op.value).to be_a Float
   end
 
   it 'reg_bits' do
+    # fmovem  fp0, d0
+    op = op_of("\xfa\x00\xe0\x01", 0)
+    expect(op.reg_bits?).to be true
+    expect(op.value).to be(1 << 16)
   end
 
   it 'reg_pair' do
+    # divs.l d0, d1:d7
+    op = op_of("\x4c\x40\x7c\x01", 1)
+    expect(op.reg_pair?).to be true
+    expect(@cs.reg_name(op.value[:reg_0] + 1)).to eq 'd1'
+    expect(@cs.reg_name(op.value[:reg_1] + 1)).to eq 'd7'
   end
 
   it 'br_disp' do
+    # fdbf     d0, $8
+    op = op_of("\xf0\x48\x00\x00\x00\x04", 1)
+    expect(op.br_disp?).to be true
+    expect(op.value[:disp]).to be 6
+    expect(op.value[:disp_size]).to be Crabstone::M68K::OP_BR_DISP_SIZE_WORD
   end
 
   it 'address_mode' do
