fc4df17d15d46d6ee061f55d9e985af20dce3acb
david942j <david942j@gmail.com>
2020-05-22 19:01:17 +0800

    Upgrade dependencies

--- lib/crabstone/arch.rb
+++ lib/crabstone/arch.rb
@@ -4,7 +4,7 @@ require 'crabstone/constants'
 require 'crabstone/cs_version'
 
 # require all files under 'crabstone/arch/<cs_major_version>'
-Dir.glob(File.join(__dir__, 'arch', Crabstone.cs_major_version.to_s, '*.rb')).each do |f|
+Dir.glob(File.join(__dir__, 'arch', Crabstone.cs_major_version.to_s, '*.rb')).sort.each do |f|
   require f
 end
 
