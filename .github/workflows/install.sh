23f0f2a38affff8e3a478641a03ab4fd4acde799
david942j <david942j@gmail.com>
2021-01-17 21:24:03 +0800

    Update build config

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -16,6 +16,7 @@ setup_linux()
 {
   # install capstone
   # There's libcapstone3 available on apt, but we want to test against a newer version.
+  sudo apt install make
   install_from_src capstone 'https://github.com/aquynh/capstone/archive/4.0.2.tar.gz'
 }
 
@@ -23,7 +24,6 @@ setup_osx()
 {
   # install capstone
   brew install capstone
-  export DYLD_LIBRARY_PATH=/usr/local/opt/capstone/lib:$DYLD_LIBRARY_PATH
 }
 
 if [[ "$CI_OS_NAME" == "macOS" ]]; then
